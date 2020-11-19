7a2a9c6b8771f974decd3a041119090a25f8a875
Tristan Wyatt <69435758+tristan0508@users.noreply.github.com>
2020-11-19 14:30:10 -0500

    initial push

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fruits = new List<string>() {"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"};
+            
+                IEnumerable<string> LFruits = fruits.Where(fruit => fruit.StartsWith("L")).ToList();
+                foreach(string fruit in LFruits)
+                {
+                    Console.WriteLine(fruit);
+                }
+
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(number => number % 4 == 0 || number % 6 == 0);
+            foreach(int number in fourSixMultiples)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+        
+    }
+}
